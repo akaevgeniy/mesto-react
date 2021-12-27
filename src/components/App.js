0f3ed328cd849de44634f845951fc65dc7ef223f
Evgeniy Sergeev <akaevgeniy@yandex.ru>
2021-12-27 13:31:10 +0300

    add components folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './images/header-logo.svg';
-import avatar from './images/profile-avatar.png';
-import edit_avatar from './images/profile-avatar-button.svg';
+import logo from '../images/header-logo.svg';
+import avatar from '../images/profile-avatar.png';
+import edit_avatar from '../images/profile-avatar-button.svg';
 
 function App() {
   return (
